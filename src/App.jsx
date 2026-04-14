59939f559a12e782829d743f04e46cd138e0f0cd
Singaa2112 <shashank007vns@gmail.com>
2026-04-14 10:55:07 +0530

    Added Navbar and Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+
+import Banner from "./Components/Banner/Banner";
+import CoinTable from "./Components/CoinTable/CoinTable";
+import NavBar from "./Components/NavBar/Navbar";
+
 function App() {
+
   return (
-    <div className="min-h-screen bg-base-100 p-10">
-      <h1 className="text-3xl font-bold text-primary mb-4">Crypto Tracker</h1>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary ml-2">Secondary</button>
-    </div>
+    <>
+    <NavBar />
+    <Banner />
+      <CoinTable />
+    </>
   );
 }
 
---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import BannerImage from "../../assets/banner1.jpeg";
+function Banner() {
+  return (
+    <div className="w-full h-[25rem] relative">
+      <img src={BannerImage} className="w-full h-full " />
+      <div className="absolute top-20 left-0 right-0 mx-auto w-[20rem]">
+        <div className="flex flex-col gap-4">
+            <div className="font-semibold text-5xl text-white">
+                Crypto Tracker
+            </div>
+            <div className="font-semibold text-sm text-white text-center">
+                Get all the Info regarding your favorite Crypto Currency
+            </div>
+        </div>
+      </div>
+    </div>
+    
+  );
+}
+export default Banner;
