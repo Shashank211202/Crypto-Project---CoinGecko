95587191b99e70234229226d8acc9772bb184db4
Singaa2112 <shashank007vns@gmail.com>
2026-04-14 17:30:41 +0530

    Added react query config

--- src/Components/CoinTable/CoinTable.jsx
+++ src/Components/CoinTable/CoinTable.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import { fetchCoinData } from "../../services/fetchCoinData";
+import {  useQuery } from "@tanstack/react-query";
+
 function CoinTable() {
+    const [page, setPage] = useState(1);
+    //Latest version of react-query uses queryKey and queryFn instead of key and fn
+   const { data, isLoading, isError, error } = useQuery({
+  queryKey: ["coins", page],
+  queryFn: () => fetchCoinData(page, "usd"),
+  retry: 2,
+  retryDelay: 1000,
+  gcTime: 1000 * 60 * 2, // updated name
+  
+});
+useEffect(() => {
+    console.log(data);
+}, [data]);
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+    if (isError) {
+        return <div>Error: {error.message}</div>;
+    }
 return(
+   
+    
   <>
-    Coin Table
+    Coin Table  <button onClick={()=> setPage(page+1)}>Click</button> {page}
   </>
 );
 }
--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { COINGECKO_API_URL } from "./constants";
+
 
 const axiosInstance = axios.create({
     baseURL: COINGECKO_API_URL,
